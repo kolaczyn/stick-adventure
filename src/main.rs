d82a0cf97eef4501459c8f011501dca9dfb0c8c4
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-03-18 23:24:30 +0100

    add scoreboard

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,11 @@ fn main() {
         .add_startup_system(setup)
         .add_startup_system(setup_player)
         .add_startup_system(setup_sticks)
+        .add_startup_system(setup_score)
+        .add_startup_system(setup_score_text)
         .add_system(move_player_system)
         .add_system(check_player_stick_collision_system)
+        .add_system(update_score_text_system)
         .run();
 }
 
@@ -45,6 +48,54 @@ impl StickBundle {
             },
         }
     }
+}
+
+#[derive(Component)]
+struct Score {
+    value: u32,
+}
+
+#[derive(Component)]
+struct ScoreText;
+
+fn setup_score(mut commands: Commands) {
+    commands.spawn((Score { value: 0 },));
+}
+
+fn setup_score_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        TextBundle {
+            text: Text {
+                sections: vec![TextSection {
+                    value: format!("Score: {}", 0),
+                    style: TextStyle {
+                        font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                        font_size: 40.0,
+                        color: Color::WHITE,
+                    },
+                }],
+                ..default()
+            },
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    left: Val::Px(5.0),
+                    top: Val::Px(5.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        ScoreText,
+    ));
+}
+
+fn update_score_text_system(score_query: Query<&Score>, mut score_text_query: Query<&mut Text>) {
+    let score = score_query.single();
+    println!("Score: {}", score.value);
+    let mut score_text = score_text_query.single_mut();
+    score_text.sections[0].value = format!("Score: {}", score.value);
 }
 
 fn setup(mut commands: Commands) {
@@ -98,6 +149,7 @@ fn check_player_stick_collision_system(
     mut commands: Commands,
     mut player_query: Query<(&Player, &Transform)>,
     mut stick_query: Query<(&Stick, &Transform, Entity)>,
+    mut score_query: Query<&mut Score>,
 ) {
     for (_player, player_transform) in player_query.iter_mut() {
         for (_stick, stick_transform, stick_entity) in stick_query.iter_mut() {
@@ -111,6 +163,7 @@ fn check_player_stick_collision_system(
                     < stick_transform.translation.y + STICK_HEIGHT / 2.0
             {
                 commands.entity(stick_entity).despawn();
+                score_query.single_mut().value += 1;
             }
         }
     }
